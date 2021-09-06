2b3ad9e2831fa3db3e13dea4495d5930f88baa1c
shubham <shubham14bajpai@gmail.com>
2021-09-06 17:47:35 +0530

    Adding new rpcs
    Signed-off-by: shubham <shubham14bajpai@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 
 use storage::storage_server::{Storage, StorageServer};
-use storage::{CreatePoolRequest, Pool};
+use storage::{CreatePoolRequest, DestroyPoolRequest, ListPoolsReply, Null, Pool, PoolType};
 
 pub mod storage {
     tonic::include_proto!("storage");
@@ -15,15 +15,40 @@ impl Storage for MyStorage {
     async fn create_pool(
         &self,
         request: Request<CreatePoolRequest>,
-    ) -> Result<Response<Pool>, Status> { 
-        println!("Got a request: {:?}", request);
-        
+    ) -> Result<Response<Pool>, Status> {
+        println!("Got a create request: {:?}", request);
+
         let args = request.into_inner();
-        
-        let reply = storage::Pool {
+
+        let reply = Pool {
             name: args.name,
             disk: args.disk,
             pooltype: args.pooltype,
+        };
+        Ok(Response::new(reply))
+    }
+
+    async fn destroy_pool(
+        &self,
+        request: Request<DestroyPoolRequest>,
+    ) -> Result<Response<Null>, Status> {
+        println!("Got a destroy request: {:?}", request);
+
+        let _args = request.into_inner();
+
+        let reply = Null {};
+        Ok(Response::new(reply))
+    }
+
+    async fn list_pools(&self, request: Request<Null>) -> Result<Response<ListPoolsReply>, Status> {
+        println!("Got a list request: {:?}", request);
+
+        let reply = ListPoolsReply {
+            pools: vec![Pool {
+                name: "tpool".to_string(),
+                disk: vec![],
+                pooltype: PoolType::Lvm as i32,
+            }],
         };
         Ok(Response::new(reply))
     }
